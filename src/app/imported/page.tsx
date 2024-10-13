bdc7dd78798056b15e9efea1a4337be7ed74c9fb
hebehh <hebehh@gmail.com>
2024-10-12 21:18:47 -0700

    tidying

--- src/app/imported/page.tsx
+++
@@ -1,6 +0,0 @@
-import Test from "@/markdown/test.mdx";
-import MDXComponents from "@/components/MDXComponents";
-
-export default function Page() {
-    return <Test components={MDXComponents} />;
-}
